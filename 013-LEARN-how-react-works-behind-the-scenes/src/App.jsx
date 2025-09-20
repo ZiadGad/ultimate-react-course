fe32c27515d852135f091e6a5635bda907979ba6
Ziad Gad <ziadamrjad@gmail.com>
2025-09-20 13:06:42 +0300

    prove state batching, and updating it is asnyc

--- 013-LEARN-how-react-works-behind-the-scenes/src/App.jsx
+++ 013-LEARN-how-react-works-behind-the-scenes/src/App.jsx
@@ -66,8 +66,16 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0); //updated after the re-render
+    console.log(likes);
   }
 
   return (
@@ -88,7 +96,7 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
